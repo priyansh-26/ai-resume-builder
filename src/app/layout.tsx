3e10b4e47b129197e29b319ad5ac0042327131cb
Priyansh khanna <145784273+priyansh-26@users.noreply.github.com>
2025-01-25 22:33:35 +0530

    new work

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s - AI Resume Builder",
+    absolute: "AI Resume Builder",
+  },
+  description:
+    "AI Resume Builder is the easiest way to create a professional resume that will help you land your dream job.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+}
