3c7534db0cd31b43915468f0fc8457bc9329f8ff
Priyansh khanna <145784273+priyansh-26@users.noreply.github.com>
2025-01-29 21:53:35 +0530

    new

---
+++ src/app/(main)/editor/forms/GeneralInfoForm.tsx
@@ -0,0 +1,41 @@
+import { generalInfoSchema, GeneralInfoValues } from "@/lib/validation"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form,FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+export default function GeneralInfoForm(){
+
+    const form =useForm<GeneralInfoValues>({
+        resolver: zodResolver(generalInfoSchema),
+        defaultValues:{
+            title:"",
+            description:"",
+        }
+    })
+    return <div className="max-w-xl mx-auto space-y-6">
+        <div className="space-y-1.5 text-center">
+            <h2 className="text-2xl font-semibold">General Info</h2>
+            <p className="text-sm text-muted-foreground">
+                This will not appear on your resume
+            </p>
+        </div>
+        <Form {...form}>
+            <form className="space-y-3">
+                <FormField
+                    control={form.control}
+                    name="title"
+                    render={({field})=>(
+                        <FormItem>
+                            <FormLabel>Project Name</FormLabel>
+                            <FormControl>
+                                <Input {...field} placeholder="My cool resume" autoFocus />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+            </form>
+        </Form>
+    </div>
+}
